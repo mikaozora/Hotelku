f83cd2ec422fb4ace4d0b92592c8e1f090506a49
satmika ozora <mikaozora08@gmail.com>
2023-01-18 12:43:31 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Receptionist recept = new Receptionist("mika", 101, "sentul");
-        recept.view();
-        System.out.println("hellooo");
-        System.out.println("Joelll");
-        int ma = 4;
-        int m = 3;
-        System.out.println(ma+m);
+
     }
 }
