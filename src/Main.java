8faf25916444b9f3bea7cd0f1b94c9bc2538f77b
satmika ozora <mikaozora08@gmail.com>
2023-01-18 20:01:07 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test 1");
-        System.out.println("helloooooooo");
-        System.out.println("uuuuuu");
-        System.out.println("testttt");
-        System.out.println("aaaaaaaaaaa");
+
     }
 }
