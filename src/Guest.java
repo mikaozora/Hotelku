a205855133c63cbe5446820dfe7beb5e815e0bcc
abdhy <abdhysamoedra030@gmail.com>
2023-01-19 08:16:15 +0700

    new update

--- src/Guest.java
+++ src/Guest.java
@@ -1,3 +1,107 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Guest {
+    private String guestName;
+    private String guestAddress;
+    private String guestId;
+    private String guestPhone;
+    private int guestRoom;
+    private int guestAge;
+    private String guestGender;
+
+    Guest(String guestName, String guestAddress, String guestId, String guestPhone, int guestRoom, int guestAge, String guestGender) {
+        this.setGuestName(guestName);
+        this.setGuestAddress(guestAddress);
+        this.guestId = "" + (long) (Math.random() * 2000000000L);
+        this.setGuestPhone(guestPhone);
+        this.setGuestRoom(guestRoom);
+        this.setGuestAge(guestAge);
+        this.setGuestGender(guestGender);
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getGuestAddress() {
+        return guestAddress;
+    }
+
+    public void setGuestAddress(String guestAddress) {
+        this.guestAddress = guestAddress;
+    }
+
+    public String getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(String guestId) {
+        this.guestId = guestId;
+    }
+
+    public String getGuestPhone() {
+        return guestPhone;
+    }
+
+    public void setGuestPhone(String guestPhone) {
+        this.guestPhone = guestPhone;
+    }
+
+    public int getGuestRoom() {
+        return guestRoom;
+    }
+
+    public void setGuestRoom(int guestRoom) {
+        this.guestRoom = guestRoom;
+    }
+
+    public int getGuestAge() {
+        return guestAge;
+    }
+
+    public void setGuestAge(int guestAge) {
+        this.guestAge = guestAge;
+    }
+
+    public String getGuestGender() {
+        return guestGender;
+    }
+
+    public void setGuestGender(String guestGender) {
+        this.guestGender = guestGender;
+    }
+
+    List<Guest> guestsArr = new ArrayList<>();
+
+    public void viewAll(String guestName, String guestAddress, String guestId, String guestPhone, int guestRoom, int guestAge, String guestGender) {
+        guestsArr.forEach(cetak -> {
+            System.out.println("Guest Name" + guestName+"Guest Address" + guestAddress + "Guest Id" + guestId + "Guest Phone" + guestPhone
+                    + "Guest Room" + guestRoom + "Guest Age" + guestAge + "Guest Gender" + guestGender);
+        });
+    }
+
+    public void addGuest(String guestName, String guestAddress, String guestId, String guestPhone, int guestRoom, int guestAge, String guestGender) {
+        guestsArr.add(new Guest(guestName, guestAddress, guestId, guestPhone, guestRoom, guestAge, guestGender));
+    }
+
+    public void updateGuest(int index,String guestName, String guestAddress, String guestId, String guestPhone, int guestRoom, int guestAge, String guestGender) {
+        guestsArr.get(index-1).setGuestName(guestName);
+        guestsArr.get(index-1).setGuestAddress(guestAddress);
+        guestsArr.get(index-1).setGuestId(guestId);
+        guestsArr.get(index-1).setGuestPhone(guestPhone);
+        guestsArr.get(index-1).setGuestRoom(guestRoom);
+        guestsArr.get(index-1).setGuestAge(guestAge);
+        guestsArr.get(index-1).setGuestGender(guestGender);
+    }
+
+    public void deleteGuest(int index){
+        guestsArr.remove(index-1);
+    }
+
 
 }
--- src/Receptionist.java
+++ src/Receptionist.java
@@ -1,3 +1,106 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Receptionist {
+    private String recId;
+    private String recName;
+    private String recUsername;
+    private String recPassword;
+    private String recAddress;
+    private String recPhone;
+    private int recAge;
+
+    Receptionist(String recId, String recName, String recUsername, String recPassword, String recAddress, String recPhone, int recAge) {
+        this.recId = "" + (long) (Math.random() * 2000000000L);
+        this.setRecName(recName);
+        this.setRecUsername(recUsername);
+        this.setRecPassword(recPassword);
+        this.setRecPassword(recPassword);
+        this.setRecAddress(recAddress);
+        this.setRecPhone(recPhone);
+        this.setRecAge(recAge);
+    }
+
+    public String getRecId() {
+        return recId;
+    }
+
+    public void setRecId(String recId) {
+        this.recId = recId;
+    }
+
+    public String getRecName() {
+        return recName;
+    }
+
+    public void setRecName(String recName) {
+        this.recName = recName;
+    }
+
+    public String getRecUsername() {
+        return recUsername;
+    }
+
+    public void setRecUsername(String recUsername) {
+        this.recUsername = recUsername;
+    }
+
+    public String getRecPassword() {
+        return recPassword;
+    }
+
+    public void setRecPassword(String recPassword) {
+        this.recPassword = recPassword;
+    }
+
+    public String getRecAddress() {
+        return recAddress;
+    }
+
+    public void setRecAddress(String recAddress) {
+        this.recAddress = recAddress;
+    }
+
+    public String getRecPhone() {
+        return recPhone;
+    }
+
+    public void setRecPhone(String recPhone) {
+        this.recPhone = recPhone;
+    }
+
+    public int getRecAge() {
+        return recAge;
+    }
+
+    public void setRecAge(int recAge) {
+        this.recAge = recAge;
+    }
+
+    List<Receptionist> rcpArr = new ArrayList<>();
+
+    public void addReceptionist(String recId, String recName, String recUsername, String recPassword, String recAddress, String recPhone, int recAge){
+        rcpArr.add(new Receptionist(recId,recName,recUsername,recPassword,recAddress,recPhone,recAge));
+    }
+
+    public void updateReceptionist(int index, String recName, String recUsername, String recPassword, String recAddress, String recPhone, int recAge){
+        rcpArr.get(index-1).setRecName(recName);
+        rcpArr.get(index-1).setRecUsername(recUsername);
+        rcpArr.get(index-1).setRecPassword(recPassword);
+        rcpArr.get(index-1).setRecAddress(recAddress);
+        rcpArr.get(index-1).setRecPhone(recPhone);
+        rcpArr.get(index-1).setRecAge(recAge);
+    }
+
+    public void deleteReceptionist(int index){
+        rcpArr.remove(index-1);
+    }
+
+    public void viewAll(String recId, String recName, String recUsername, String recPassword, String recAddress, String recPhone, int recAge) {
+        rcpArr.forEach(cetak -> {
+            System.out.println("Receptionist Id" + recId +"Receptionist Name" +recName +"Receptionist Username" + recUsername + "Receptionist Password" + recPassword
+                    + "Receptionist Address" + recAddress + "Receptionist Phone" + recPhone + "Receptionist Age" + recAge);
+        });
+    }
 
 }
