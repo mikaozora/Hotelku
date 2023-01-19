f274321171c1646fc50c55c7c07de6a805fa5d36
satmika ozora <mikaozora08@gmail.com>
2023-01-19 09:04:14 +0700

    update menu, create crud admin

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Admin {
+    String adminId;
+    String username;
+    String password;
+    String adminName;
+    int adminPhoneNum;
+    String adminAddress;
+
+    List<Admin> adminList = new ArrayList<>();
+
+    Admin(String username, String password, String adminName, int adminPhoneNum, String adminAddress){
+        this.adminId = "" + ((long)(Math.random() * (9999999999L - 1000000000L + 1)) + 1000000000L);
+        this.setUsername(username);
+        this.setPassword(password);
+        this.setAdminName(adminName);
+        this.setAdminPhoneNum(adminPhoneNum);
+        this.setAdminAddress(adminAddress);
+    }
+
+    public void viewAll(){
+        AtomicInteger i = new AtomicInteger(1);
+        adminList.forEach(admin -> {
+
+            System.out.print(i + ". ");
+            System.out.println("Id: " + admin.getAdminId());
+            System.out.println("Name: " + admin.getAdminName());
+            System.out.println("Username: " + admin.getUsername());
+            System.out.println("Phone Number: " + admin.getAdminPhoneNum());
+            System.out.println("Address: " + admin.getAdminAddress());
+            i.getAndIncrement();
+        });
+    }
+
+    void addAdmin(String username, String password, String adminName, int adminPhoneNum, String adminAddress){
+        adminList.add(new Admin(username, password, adminName, adminPhoneNum, adminAddress));
+    }
+
+    void updateAdmin(int index, String username, String password, String adminName, int adminPhoneNum, String adminAddress){
+        adminList.get(index-1).setUsername(username);
+        adminList.get(index-1).setPassword(password);
+        adminList.get(index-1).setAdminName(adminName);
+        adminList.get(index-1).setAdminPhoneNum(adminPhoneNum);
+        adminList.get(index-1).setAdminAddress(adminAddress);
+    }
+    void deleteAdmin(int index){
+        adminList.remove(index-1);
+    }
+
+    void initAdmin(){
+        adminList.add(new Admin("admin", "admin", "admin1", 12345, "sentul"));
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public int getAdminPhoneNum() {
+        return adminPhoneNum;
+    }
+
+    public void setAdminPhoneNum(int adminPhoneNum) {
+        this.adminPhoneNum = adminPhoneNum;
+    }
+
+    public String getAdminAddress() {
+        return adminAddress;
+    }
+
+    public void setAdminAddress(String adminAddress) {
+        this.adminAddress = adminAddress;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,135 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
-    static void menuAdmin() {
+    static Admin admin = new Admin(null, null, null, 0, null);
+    static void adminMenu(){
+        int choose, phoneNum, index;
+        String username, password, address, name;
+
+        do {
+            System.out.println("1. View admin");
+            System.out.println("2. Add admin");
+            System.out.println("3. Update admin");
+            System.out.println("4. Delete admin");
+            System.out.println("5. Exit");
+            System.out.print(">> ");
+            choose = input.nextInt();
+            input.nextLine();
+            switch (choose){
+                case 1:
+                    admin.viewAll();
+                    break;
+                case 2:
+                    System.out.println("====Add Admin======");
+                    System.out.print("Username: ");
+                    username = input.nextLine();
+                    System.out.print("Password: ");
+                    password = input.nextLine();
+                    System.out.print("Name: ");
+                    name = input.nextLine();
+                    System.out.print("Phone number: ");
+                    phoneNum = input.nextInt();
+                    input.nextLine();
+                    System.out.print("Address: ");
+                    address = input.nextLine();
+                    admin.addAdmin(username, password, name, phoneNum, address);
+                    break;
+                case 3:
+                    admin.viewAll();
+                    System.out.print("Choose data you want to change: ");
+                    index = input.nextInt();
+                    input.nextLine();
+                    System.out.println("====Change Data Admin======");
+                    System.out.print("Username: ");
+                    username = input.nextLine();
+                    System.out.print("Password: ");
+                    password = input.nextLine();
+                    System.out.print("Name: ");
+                    name = input.nextLine();
+                    System.out.print("Phone number: ");
+                    phoneNum = input.nextInt();
+                    input.nextLine();
+                    System.out.print("Address: ");
+                    address = input.nextLine();
+                    admin.updateAdmin(index, username, password, name, phoneNum, address);
+                    break;
+                case 4:
+                    admin.viewAll();
+                    System.out.print("Choose data you want to delete: ");
+                    index = input.nextInt();
+                    input.nextLine();
+                    admin.deleteAdmin(index);
+                    System.out.println("Successfully delete admin");
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+        }while(choose != 5);
+    }
+
+    static void employeeMenu(){
+        int choose;
+        do {
+            System.out.println("1. View employee");
+            System.out.println("2. Add employee");
+            System.out.println("3. Update employee");
+            System.out.println("4. Delete employee");
+            System.out.println("5. Exit");
+            System.out.print(">> ");
+            choose = input.nextInt();
+            input.nextLine();
+            switch (choose){
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+        }while(choose != 5);
+    }
+
+    static void roomMenu(){
+        int choose;
+        do {
+            System.out.println("1. View room");
+            System.out.println("2. Add room");
+            System.out.println("3. Update room");
+            System.out.println("4. Delete room");
+            System.out.println("5. Exit");
+            System.out.print(">> ");
+            choose = input.nextInt();
+            input.nextLine();
+            switch (choose){
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+        }while(choose != 5);
+    }
+
+    static void homeAdmin() {
 
         int choose;
         do {
@@ -10,17 +138,32 @@ public class Main {
             System.out.println("1. Receptionist");
             System.out.println("2. Employee");
             System.out.println("3. Rooms");
-            System.out.println("4. Logout");
+            System.out.println("4. Admin");
+            System.out.println("5. Logout");
             System.out.print(">> ");
             choose = input.nextInt();
             input.nextLine();
-
-        } while (choose != 4);
+            switch (choose){
+                case 1:
+                    break;
+                case 2:
+                    employeeMenu();
+                    break;
+                case 3:
+                    roomMenu();
+                    break;
+                case 4:
+                    adminMenu();
+                    break;
+                default:
+                    break;
+            }
+        } while (choose != 5);
         formLogin();
 
     }
 
-    static void menuRec(){
+    static void homeRec(){
 
     }
 
@@ -37,13 +180,15 @@ public class Main {
         boolean res = true;
 
         if(res){
-            menuAdmin();
+            homeAdmin();
         }else{
-            menuRec();
+            homeRec();
         }
 
     }
     public static void main(String[] args) {
+        admin.initAdmin();
         formLogin();
+
     }
 }
