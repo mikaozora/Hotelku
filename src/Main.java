f10875813808047d56d1d1f1c0173836c3dea621
satmika ozora <mikaozora08@gmail.com>
2023-01-19 21:47:20 +0700

    create menu to some function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.nio.file.AccessDeniedException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +9,60 @@ public class Main {
     static Guest guest = new Guest(null,null,null,0,0,null);
     static Room room = new Room(0, null, 0, false);
     static int indexLogin = -1;
-    static void adminMenu(){
-        int choose, phoneNum, index;
+
+    static void addAdmin(){
+        int phoneNum;
         String username, password, address, name;
+        System.out.println("====Add Admin======");
+        System.out.print("Username: ");
+        username = input.nextLine();
+        System.out.print("Password: ");
+        password = input.nextLine();
+        System.out.print("Name: ");
+        name = input.nextLine();
+        System.out.print("Phone number: ");
+        phoneNum = input.nextInt();
+        input.nextLine();
+        System.out.print("Address: ");
+        address = input.nextLine();
+        admin.addAdmin(username, password, name, phoneNum, address);
+    }
+    static void updateAdmin(){
+        int phoneNum;
+        String username, password, address, name;
+        System.out.println("====Change Data Admin======");
+        System.out.print("Username: ");
+        username = input.nextLine();
+        System.out.print("Password: ");
+        password = input.nextLine();
+        System.out.print("Name: ");
+        name = input.nextLine();
+        System.out.print("Phone number: ");
+        phoneNum = input.nextInt();
+        input.nextLine();
+        System.out.print("Address: ");
+        address = input.nextLine();
+        admin.updateAdmin(indexLogin, username, password, name, phoneNum, address);
+    }
+    static void deleteAdmin(){
+        int index;
+        admin.viewAll();
+        System.out.print("Choose data you want to delete: ");
+        index = input.nextInt();
+        input.nextLine();
+        if (index - 1 == indexLogin) {
+            admin.deleteAdmin(index);
+            System.out.println("Successfully delete admin");
+            indexLogin = -1;
+            formLogin();
+
+        } else {
+            admin.deleteAdmin(index);
+            System.out.println("Successfully delete admin");
+        }
+    }
+    static void adminMenu(){
+        int choose;
 
         do {
             System.out.println("1. View admin");
@@ -26,50 +78,13 @@ public class Main {
                     admin.viewAll();
                     break;
                 case 2:
-                    System.out.println("====Add Admin======");
-                    System.out.print("Username: ");
-                    username = input.nextLine();
-                    System.out.print("Password: ");
-                    password = input.nextLine();
-                    System.out.print("Name: ");
-                    name = input.nextLine();
-                    System.out.print("Phone number: ");
-                    phoneNum = input.nextInt();
-                    input.nextLine();
-                    System.out.print("Address: ");
-                    address = input.nextLine();
-                    admin.addAdmin(username, password, name, phoneNum, address);
+                    addAdmin();
                     break;
                 case 3:
-                    System.out.println("====Change Data Admin======");
-                    System.out.print("Username: ");
-                    username = input.nextLine();
-                    System.out.print("Password: ");
-                    password = input.nextLine();
-                    System.out.print("Name: ");
-                    name = input.nextLine();
-                    System.out.print("Phone number: ");
-                    phoneNum = input.nextInt();
-                    input.nextLine();
-                    System.out.print("Address: ");
-                    address = input.nextLine();
-                    admin.updateAdmin(indexLogin, username, password, name, phoneNum, address);
+                    updateAdmin();
                     break;
                 case 4:
-                    admin.viewAll();
-                    System.out.print("Choose data you want to delete: ");
-                    index = input.nextInt();
-                    input.nextLine();
-                    if (index - 1 == indexLogin) {
-                        admin.deleteAdmin(index);
-                        System.out.println("Successfully delete admin");
-                        indexLogin = -1;
-                        formLogin();
-
-                    } else {
-                        admin.deleteAdmin(index);
-                        System.out.println("Successfully delete admin");
-                    }
+                    deleteAdmin();
                     break;
                 case 5:
                     break;
@@ -79,6 +94,61 @@ public class Main {
         } while (choose != 5);
     }
 
+    static void addEmployee(){
+        String name, phoneNum, address, role;
+        System.out.println("====Add Employee======");
+        //Input Name
+        System.out.print("Name: ");
+        name = input.nextLine();
+        //Input Role
+        System.out.println("Role: ");
+        role = input.nextLine();
+        //Input Phone Number
+        System.out.print("Phone number: ");
+        phoneNum = input.nextLine();
+        //Input Address
+        System.out.print("Address: ");
+        address = input.nextLine();
+
+        employee.addEmployee(name, address, phoneNum, role);
+
+        System.out.println("Employee's data has successfully added");
+    }
+    static void updateEmployee(){
+        String name, phoneNum, address, role;
+        int index;
+        employee.viewAll();
+        System.out.println("Choose data you want to change");
+        index = input.nextInt();
+        input.nextLine();
+        System.out.println("====Change Data Employee======");
+        //Update Name
+        System.out.print("Name: ");
+        name = input.nextLine();
+        //Update Role
+        System.out.println("Role: ");
+        role = input.nextLine();
+        //Update Phone Number
+        System.out.print("Phone number: ");
+        phoneNum = input.nextLine();
+        //Update Address
+        System.out.print("Address: ");
+        address = input.nextLine();
+
+        employee.updateEmployee(index, name, address, phoneNum, role);
+        System.out.println("Employee Has Been Updated!");
+    }
+    static void deleteEmployee(){
+        int index;
+        employee.viewAll();
+        System.out.println("====Delete Data Employee======");
+        //Input data employee to delete
+        System.out.print("Choose data you want to delete: ");
+        index = input.nextInt();
+        input.nextLine();
+        employee.deleteEmployee(index);
+        System.out.println("Successfully delete employee");
+    }
     static void employeeMenu() {
         String name, phoneNum, address, role;
         int choose, index;
@@ -97,56 +167,13 @@ public class Main {
                     employee.viewAll();
                     break;
                 case 2:
-                    System.out.println("====Add Employee======");
-                    //Input Name
-                    System.out.print("Name: ");
-                    name = input.nextLine();
-                    //Input Role
-                    System.out.println("Role: ");
-                    role = input.nextLine();
-                    //Input Phone Number
-                    System.out.print("Phone number: ");
-                    phoneNum = input.nextLine();
-                    //Input Address
-                    System.out.print("Address: ");
-                    address = input.nextLine();
-
-                    employee.addEmployee(name, address, phoneNum, role);
-
-                    System.out.println("Employee's data has successfully added");
+                    addEmployee();
                     break;
                 case 3:
-                    employee.viewAll();
-                    System.out.println("Choose data you want to change");
-                    index = input.nextInt();
-                    input.nextLine();
-                    System.out.println("====Change Data Employee======");
-                    //Update Name
-                    System.out.print("Name: ");
-                    name = input.nextLine();
-                    //Update Role
-                    System.out.println("Role: ");
-                    role = input.nextLine();
-                    //Update Phone Number
-                    System.out.print("Phone number: ");
-                    phoneNum = input.nextLine();
-                    //Update Address
-                    System.out.print("Address: ");
-                    address = input.nextLine();
-
-                    employee.updateEmployee(index, name, address, phoneNum, role);
-                    System.out.println("Employee Has Been Updated!");
-
+                    updateEmployee();
                     break;
                 case 4:
-                    employee.viewAll();
-                    System.out.println("====Delete Data Employee======");
-                    //Input data employee to delete
-                    System.out.print("Choose data you want to delete: ");
-                    index = input.nextInt();
-                    input.nextLine();
-                    employee.deleteEmployee(index);
-                    System.out.println("Successfully delete employee");
+                    deleteEmployee();
                     break;
                 case 5:
                     break;
@@ -156,9 +183,58 @@ public class Main {
         }while(choose != 5);
     }
 
-    static void receptionistMenu() {
-        int choose, index,age;
+    static void addReceptionist(){
+        int age;
         String phoneNum, username, password, address, name;
+        System.out.println("====Add Receptionist======");
+        System.out.print("Username: ");
+        username = input.nextLine();
+        System.out.print("Password: ");
+        password = input.nextLine();
+        System.out.print("Name: ");
+        name = input.nextLine();
+        System.out.print("Phone number: ");
+        phoneNum = input.nextLine();
+        input.nextLine();
+        System.out.print("Address: ");
+        address = input.nextLine();
+        System.out.print("Age: ");
+        age = input.nextInt();
+        receptionist.addReceptionist(username, password, name, phoneNum, address, age);
+    }
+    static void updateReceptionist(){
+        int age, index;
+        String phoneNum, username, password, address, name;
+        receptionist.viewAll();
+        System.out.print("Choose data you want to change: ");
+        index = input.nextInt();
+        input.nextLine();
+        System.out.println("====Change Data Receptionist======");
+        System.out.print("Username: ");
+        username = input.nextLine();
+        System.out.print("Password: ");
+        password = input.nextLine();
+        System.out.print("Name: ");
+        name = input.nextLine();
+        System.out.print("Phone number: ");
+        phoneNum = input.nextLine();
+        System.out.print("Address: ");
+        address = input.nextLine();
+        System.out.print("Age: ");
+        age = input.nextInt();
+        receptionist.updateReceptionist(index, username, password, name, phoneNum, address, age);
+    }
+    static void deleteReceptionist(){
+        int index;
+        receptionist.viewAll();
+        System.out.print("Choose data you want to delete: ");
+        index = input.nextInt();
+        input.nextLine();
+        receptionist.deleteReceptionist(index);
+        System.out.println("Successfully delete admin");
+    }
+    static void receptionistMenu() {
+        int choose;
 
         do {
             System.out.println("1. View Receptionist");
@@ -174,49 +250,13 @@ public class Main {
                     receptionist.viewAll();
                     break;
                 case 2:
-                    System.out.println("====Add Receptionist======");
-                    System.out.print("Username: ");
-                    username = input.nextLine();
-                    System.out.print("Password: ");
-                    password = input.nextLine();
-                    System.out.print("Name: ");
-                    name = input.nextLine();
-                    System.out.print("Phone number: ");
-                    phoneNum = input.nextLine();
-                    input.nextLine();
-                    System.out.print("Address: ");
-                    address = input.nextLine();
-                    System.out.print("Age: ");
-                    age = input.nextInt();
-                    receptionist.addReceptionist(username, password, name, phoneNum, address, age);
+                    addReceptionist();
                     break;
                 case 3:
-                    receptionist.viewAll();
-                    System.out.print("Choose data you want to change: ");
-                    index = input.nextInt();
-                    input.nextLine();
-                    System.out.println("====Change Data Receptionist======");
-                    System.out.print("Username: ");
-                    username = input.nextLine();
-                    System.out.print("Password: ");
-                    password = input.nextLine();
-                    System.out.print("Name: ");
-                    name = input.nextLine();
-                    System.out.print("Phone number: ");
-                    phoneNum = input.nextLine();
-                    System.out.print("Address: ");
-                    address = input.nextLine();
-                    System.out.print("Age: ");
-                    age = input.nextInt();
-                    receptionist.updateReceptionist(index, username, password, name, phoneNum, address, age);
+                    updateReceptionist();
                     break;
                 case 4:
-                    receptionist.viewAll();
-                    System.out.print("Choose data you want to delete: ");
-                    index = input.nextInt();
-                    input.nextLine();
-                    receptionist.deleteReceptionist(index);
-                    System.out.println("Successfully delete admin");
+                    deleteReceptionist();
                     break;
                 case 5:
                     break;
@@ -226,10 +266,56 @@ public class Main {
         } while (choose != 5);
     }
 
-    static void roomMenu(){
+    static void addRoom(){
         String roomCategory;
-        int choose, roomNumber, roomPrice, index;
+        int roomNumber, roomPrice;
         boolean roomStatus;
+        System.out.println("====Add Room======");
+        System.out.print("Room Number: ");
+        roomNumber = input.nextInt();
+        input.nextLine();
+        System.out.print("Room Category: ");
+        roomCategory = input.nextLine();
+        System.out.print("Room Price: ");
+        roomPrice = input.nextInt();
+        input.nextLine();
+        System.out.print("Room Status: ");
+        roomStatus = input.nextBoolean();
+        room.addRoom(roomNumber, roomCategory, roomPrice, roomStatus);
+        System.out.println("Room has successfully added!");
+    }
+    static void updateRoom(){
+        String roomCategory;
+        int roomNumber, roomPrice, index;
+        boolean roomStatus;
+        room.viewRoom();
+        System.out.print("Choose data you want to change: ");
+        index = input.nextInt();
+        input.nextLine();
+        System.out.println("====Change Room's data======");
+        System.out.print("Room Number: ");
+        roomNumber = input.nextInt();
+        input.nextLine();
+        System.out.print("Room Category: ");
+        roomCategory = input.nextLine();
+        System.out.print("Room Price: ");
+        roomPrice = input.nextInt();
+        input.nextLine();
+        System.out.print("Room Status: ");
+        roomStatus = input.nextBoolean();
+        room.updateRoom(index, roomNumber, roomCategory, roomPrice, roomStatus);
+    }
+    static void deleteRoom(){
+        int index;
+        room.viewRoom();
+        System.out.print("Choose data you want to delete: ");
+        index = input.nextInt();
+        input.nextLine();
+        room.deleteRoom(index);
+        System.out.println("Successfully delete room");
+    }
+    static void roomMenu(){
+        int choose;
 
         do {
             System.out.println("1. View room");
@@ -245,45 +331,13 @@ public class Main {
                     Room.viewRoom();
                     break;
                 case 2:
-                    System.out.println("====Add Room======");
-                    System.out.print("Room Number: ");
-                    roomNumber = input.nextInt();
-                    input.nextLine();
-                    System.out.print("Room Category: ");
-                    roomCategory = input.nextLine();
-                    System.out.print("Room Price: ");
-                    roomPrice = input.nextInt();
-                    input.nextLine();
-                    System.out.print("Room Status: ");
-                    roomStatus = input.nextBoolean();
-                    room.addRoom(roomNumber, roomCategory, roomPrice, roomStatus);
-                    System.out.println("Room has successfully added!");
+                    addRoom();
                     break;
                 case 3:
-                    room.viewRoom();
-                    System.out.print("Choose data you want to change: ");
-                    index = input.nextInt();
-                    input.nextLine();
-                    System.out.println("====Change Room's data======");
-                    System.out.print("Room Number: ");
-                    roomNumber = input.nextInt();
-                    input.nextLine();
-                    System.out.print("Room Category: ");
-                    roomCategory = input.nextLine();
-                    System.out.print("Room Price: ");
-                    roomPrice = input.nextInt();
-                    input.nextLine();
-                    System.out.print("Room Status: ");
-                    roomStatus = input.nextBoolean();
-                    room.updateRoom(index, roomNumber, roomCategory, roomPrice, roomStatus);
+                    updateRoom();
                     break;
                 case 4:
-                    room.viewRoom();
-                    System.out.print("Choose data you want to delete: ");
-                    index = input.nextInt();
-                    input.nextLine();
-                    room.deleteRoom(index);
-                    System.out.println("Successfully delete room");
+                    deleteRoom();
                     break;
                 case 5:
                     break;
